b0c0c4ee37e25fc0c8949993e5b05859e9cc0bde
Tim Callaghan <tmcallaghan@gmail.com>
2022-01-21 15:35:32 +0000

    ensure necessary environment variable are set

--- iibench-runner.bash
+++ iibench-runner.bash
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# check for required host environment parameters
+MONGO_SERVER_TEST=${DOCDB_HOST:?Environment variable not set or empty}
+MONGO_USERNAME_TEST=${DOCDB_USERNAME:?Environment variable not set or empty}
+MONGO_PASSWORD_TEST=${DOCDB_PASSWORD:?Environment variable not set or empty}
+
+
 instanceClasses="db.r6g.large db.r6g.xlarge db.r6g.2xlarge db.r6g.4xlarge db.r6g.8xlarge db.r6g.12xlarge db.r6g.16xlarge db.r5.large db.r5.xlarge db.r5.2xlarge db.r5.4xlarge db.r5.8xlarge db.r5.12xlarge db.r5.16xlarge db.r5.24xlarge"
 #instanceClasses="db.r6g.large db.r6g.8xlarge db.r5.2xlarge"
 
